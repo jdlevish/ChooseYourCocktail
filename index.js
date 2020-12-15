d2f260f306475886883430a393861f67699152eb
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-12-15 10:46:58 -0800

    first commit added started code for index.js, installed packages , and added initial imports

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const app = express();
+
+
+app.listen(3000, () => console.log(' listening on port 3000!'));
