a3af91f05fd4417d7cfe4e16d544126b63acfe98
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-12-29 23:05:27 -0800

    changed font to playfair display, added new logo and added heading to navbar

--- client/chooseyourcocktail/src/components/Nav.js
+++ client/chooseyourcocktail/src/components/Nav.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 //Boostrap
 import { BrowserRouter as Router, Link, useHistory } from 'react-router-dom';
 import { Navbar, Nav, Image, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
-import { ReactComponent as Logo } from '../images/short cocktail.svg'
+import { ReactComponent as Logo } from '../images/cycLogo.svg'
 
 
 
@@ -21,7 +21,7 @@ class NavBarHome extends Component {
         return (
             <div>
                 <Navbar bg="light" expand="lg" >
-                    <Navbar.Brand href="#home" ><Logo className='float-left pt-0 ' /><p className=' float-right pt-4 pl-2 color-navbar  '>Choose Your Cocktail</p></Navbar.Brand>
+                    <Navbar.Brand href="#home" ><Logo className="svgLogo"></Logo><h1 className="navHeader float-right pt-3">Choose Your Cocktail</h1></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto color-navbar">
